125d9e2bc34bdb11ab4d17e2c3ffe5861456b7ce
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2017-02-23 03:58:21 +0100

    Improve log of DebugResources

--- resources/DebugResources/Debug.cs
+++ resources/DebugResources/Debug.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using System.Reflection;
 using GTANetworkServer;
 
 public class Debug : Script
@@ -23,38 +22,34 @@ public class Debug : Script
 
         foreach (string resource in resourcesNames)
         {
-            if (API.isResourceRunning(resource))
+            if (!API.isResourceRunning(resource)) continue;
+
+            string resourceName = API.getResourceName(resource);
+            ResourceType resourceType = API.getResourceType(resource);
+            string resourceAuthor = API.getResourceAuthor(resource);
+            string resourceVersion = API.getResourceVersion(resource);
+            string resourceDescription = API.getResourceDescription(resource);
+            CommandInfo[] resourceCommand = API.getResourceCommandInfos(resource);
+            string name = API.getResourceName(resource);
+
+            string resourceString = "[" + resourceType + "] " + resourceName;
+            if (resourceAuthor != " ") resourceString += (" - By : " + resourceAuthor);
+            if (resourceVersion != " ") resourceString += (" (Version " + resourceVersion + ")");
+            API.consoleOutput(resourceString);
+
+            if (resourceDescription != "")
+                API.consoleOutput("\t- Description : " + resourceDescription);
+            if (resourceCommand.Length > 0)
             {
-                string resourceName = API.getResourceName(resource);
-                ResourceType resourceType = API.getResourceType(resource);
-                string resourceAuthor = API.getResourceAuthor(resource);
-                string resourceVersion = API.getResourceVersion(resource);
-                string resourceDescription = API.getResourceDescription(resource);
-                CommandInfo[] resourceCommand = API.getResourceCommandInfos(resource);
-                string name = API.getResourceName(resource);
-
-                string resourceString = "[" + resourceType + "] " + resourceName;
-                if (resourceAuthor != "") resourceString += (" - By : " + resourceAuthor);
-                if (resourceVersion != "") resourceString += (" (Version " + resourceVersion + ")");
-                API.consoleOutput(resourceString);
-
-                if (resourceDescription != "")
-                    API.consoleOutput("\t- Description : " + resourceDescription);
-                if (resourceCommand.Length > 0)
+                API.consoleOutput("\t- Commands : " + resourceDescription);
+                foreach (CommandInfo command in resourceCommand)
                 {
-                    API.consoleOutput("\t- Commands : " + resourceDescription);
-                    foreach (CommandInfo command in resourceCommand)
-                    {
-                        string commandSyntax = "";
-                        foreach (ParameterInfo parameter in command.Parameters)
-                            commandSyntax += parameter.Name + ", ";
-                        commandSyntax.Remove(commandSyntax.Length - 2);
-                        API.consoleOutput("\t\t * " + command.Command + "(" + commandSyntax + ")");
-                    }
+                    string commandSyntax = command.Parameters.Aggregate("", (current, parameter) => current + (parameter.Name + ", "));
+                    API.consoleOutput("\t\t * " + command.Command + "(" + commandSyntax.Substring(0, commandSyntax.Length - 2) + ")");
                 }
-
-                API.consoleOutput("");
             }
+
+            API.consoleOutput("");
         }
 
         API.consoleOutput("------------ RESOURCES ------------\n");
